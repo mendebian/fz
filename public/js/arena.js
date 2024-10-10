62bbd0e6e30e0598971529c4f94e4aa094550549
lucas <97731292+mendebian@users.noreply.github.com>
2024-10-10 17:22:43 -0300

    looping update

--- public/js/arena.js
+++ public/js/arena.js
@@ -328,7 +328,7 @@ function gameLoop() {
     movePlayer(); 
     drawGame();  
     //requestAnimationFrame(gameLoop);
-    setTimeout(gameLoop, 1000 / 60);
+    setTimeout(gameLoop, 1000 / 120);
 }
 
 //requestAnimationFrame(gameLoop);
