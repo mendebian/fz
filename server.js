0d8e436f2e0f65d4cb5dd20c2f4e6addc84923f8
lucas <97731292+mendebian@users.noreply.github.com>
2024-10-10 17:43:57 -0300

    teste

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ let ball = {
     radius: 10,
     velocityX: 0,
     velocityY: 0,
-    friction: 0.979,
+    friction: 0.978,
     acceleration: 0.3,
     mass: 1,
     angle: 0,
@@ -98,7 +98,7 @@ io.on('connection', (socket) => {
 
         socket.on('move', (angle) => {
             const player = players[socket.id];
-            const speed = 3.2;
+            const speed = 3.6;
             
             player.x += Math.cos(angle) * speed;
             player.y += Math.sin(angle) * speed;
